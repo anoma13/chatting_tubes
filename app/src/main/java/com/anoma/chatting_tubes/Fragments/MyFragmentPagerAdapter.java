a3c765de14187cd1df513d0656c06f39a6b64605
anoma <wahyuli.dwiki.nanda@gmail.com>
2020-06-04 23:28:00 +0700

    update_1

---
+++ app/src/main/java/com/anoma/chatting_tubes/Fragments/MyFragmentPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.anoma.chatting_tubes.Fragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter{
+
+    public MyFragmentPagerAdapter(FragmentManager fragmentManager){
+        super(fragmentManager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                RequestFragment requestFragment=new RequestFragment();
+                return requestFragment;
+            case 1:
+                ChatFragment chatFragment=new ChatFragment();
+                return chatFragment;
+            case 2:
+                FriendFragment friendFragment=new FriendFragment();
+                return friendFragment;
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch(position){
+            case 0:
+                return "REQUESTS";
+            case 1:
+                return "CHATS";
+            case 2:
+                return "FRIENDS";
+            default:
+                return null;
+        }
+    }
+
+}
